221ae9ee5162a959c99034f6ef89d067fad5f212
Vihas Shah <vihasshah@gmail.com>
2018-10-18 02:41:31 +0530

    static center circle and pinch zoom enable image created

--- EditingScreen.js
+++ EditingScreen.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Image, StyleSheet, Dimensions } from 'react-native'
 import PinchZoomView from './PinchZoomView';
 
 export default class EditingScreen extends Component {
     render = () => {
         let imgUri = this.props.navigation.getParam("URI")
         return (
-            <PinchZoomView>
-                <View style={styles.imageWrapper}>
-                    <Image source={{ uri: imgUri}} style={styles.image} />
+            <View style={styles.container}>
+
+                <View style={styles.wrapper}>
+                    <PinchZoomView>
+                        <View style={styles.imageWrapper}>
+                            <Image source={{ uri: imgUri}} style={styles.image} />
+                        </View>
+                    </PinchZoomView>
                 </View>
-            </PinchZoomView>
+                <View style={styles.selectionView} pointerEvents="none"/>
+                {/* <PinchZoomView> */}
+                   
+                {/* </PinchZoomView> */}
+                
+            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    wrapper:{
+        ...StyleSheet.absoluteFillObject,
+        flex:1,
+    },
+    selectionView:{
+        width:Dimensions.get('window').width / 2,
+        height:Dimensions.get('window').width / 2,
+        borderRadius:(Dimensions.get('window').width / 2) / 2,
+        backgroundColor:"#fff6",
+        alignSelf:'center'
+    },
     imageWrapper:{
-        flex:1
+        flex:1,
     },
     image:{
         flex:1,
